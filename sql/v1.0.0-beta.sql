e8f3efd92dcf961740c5927cba6ccb9f0a0a9332
Kim Seon Woo <seonwoo960000@naver.com>
2023-11-11 13:01:32 +0900

    Add `uuid` column in `user` table

--- sql/v1.0.0-beta.sql
+++ sql/v1.0.0-beta.sql
@@ -1,47 +1,59 @@
-create table if not exists "user"
+create table public."user"
 (
     id         bigserial
-    primary key,
+        primary key,
+    uuid       uuid                                not null,
     name       varchar(100)                        not null,
     status     char      default 'N'::bpchar       not null,
     password   varchar(255)                        not null,
     created_at timestamp default CURRENT_TIMESTAMP not null,
     updated_at timestamp default CURRENT_TIMESTAMP not null
-    );
-
-comment on column "user".name is 'user name';
-
-comment on column "user".status is 'N: normal, D: deleted, A: abnormal';
-
-create unique index if not exists uidx_user_name
-    on "user" (name);
-
-create table if not exists notification
-(
-    id         bigserial
-    primary key,
-    user_id    bigint                              not null,
-    type       char                                not null,
-    body       json                                not null,
-    view_yn    char      default 'N'::bpchar       not null,
-    created_at timestamp default CURRENT_TIMESTAMP not null
 );
 
-comment on column notification.user_id is 'the target user to send notification to';
+comment on column public."user".uuid is 'public id to uniquely identify a user';
 
-comment on column notification.type is 'the type of notification e.g. C: Someone replied with comment';
+comment on column public."user".name is 'user name';
 
-comment on column notification.body is 'the main data of the notification';
+comment on column public."user".status is 'N: normal, D: deleted, A: abnormal';
 
-comment on column notification.view_yn is 'whether the user has viewed the notification(Y/N)';
+alter table public."user"
+    owner to seonwoo960000;
 
-create index if not exists idx_target_user_id
-    on notification (user_id);
+create unique index uidx_user_uuid
+    on public."user" (uuid);
 
-create table if not exists post
+create unique index uidx_user_name
+    on public."user" (name);
+
+create table public.notification
+(
+    id             bigserial
+        primary key,
+    target_user_id bigint                              not null,
+    type           char                                not null,
+    body           json                                not null,
+    view_yn        char      default 'N'::bpchar       not null,
+    created_at     timestamp default CURRENT_TIMESTAMP not null
+);
+
+comment on column public.notification.target_user_id is 'the target user to send notification to';
+
+comment on column public.notification.type is 'the type of notification e.g. C: Someone replied with comment';
+
+comment on column public.notification.body is 'the main data of the notification';
+
+comment on column public.notification.view_yn is 'whether the user has viewd the notification(Y/N)';
+
+alter table public.notification
+    owner to seonwoo960000;
+
+create index idx_target_user_id
+    on public.notification (target_user_id);
+
+create table public.post
 (
     id         bigserial
-    primary key,
+        primary key,
     user_id    bigint                                           not null,
     title      varchar(64)                                      not null,
     subtitle   varchar(100),
@@ -52,28 +64,34 @@ create table if not exists post
     deleted_yn char        default 'N'::bpchar                  not null,
     created_at timestamp   default CURRENT_TIMESTAMP            not null,
     updated_at timestamp   default CURRENT_TIMESTAMP            not null
-    );
+);
 
-comment on column post.user_id is 'maps to user.id';
+comment on column public.post.user_id is 'maps to user.id';
 
-comment on column post.category is 'category of the post, e.g. DEFAULT, KNOWLEDGE, COMMUNITY, etc';
+comment on column public.post.category is 'category of the post, e.g. DEFAULT, KNOWLEDGE, COMMUNITY, etc';
 
-comment on column post.type is 'type of the post, e.g. ARTICLE, LINK, etc';
+comment on column public.post.type is 'type of the post, e.g. ARTICLE, LINK, etc';
 
-comment on column post.deleted_yn is 'whether the post is deleted (Y/N)';
+comment on column public.post.deleted_yn is 'whether the post is deleted (Y/N)';
 
-create table if not exists post_reaction
+alter table public.post
+    owner to seonwoo960000;
+
+create table public.post_reaction
 (
     post_id       bigint                              not null,
     user_id       bigint                              not null,
     reaction_type char                                not null,
     created_at    timestamp default CURRENT_TIMESTAMP not null,
     primary key (post_id, user_id)
-    );
+);
 
-comment on column post_reaction.reaction_type is 'L: Like, D: Dislike';
+comment on column public.post_reaction.reaction_type is 'L: Like, D: Dislike';
 
-create table if not exists post_statistic
+alter table public.post_reaction
+    owner to seonwoo960000;
+
+create table public.post_statistic
 (
     post_id       bigserial,
     like_count    integer default 0 not null,
@@ -81,16 +99,19 @@ create table if not exists post_statistic
     comment_count integer default 0 not null
 );
 
-comment on column post_statistic.like_count is 'number of likes';
+comment on column public.post_statistic.like_count is 'number of likes';
 
-comment on column post_statistic.dislike_count is 'number of dislikes';
+comment on column public.post_statistic.dislike_count is 'number of dislikes';
 
-comment on column post_statistic.comment_count is 'number of comments';
+comment on column public.post_statistic.comment_count is 'number of comments';
 
-create table if not exists comment
+alter table public.post_statistic
+    owner to seonwoo960000;
+
+create table public.comment
 (
     id          bigserial
-    primary key,
+        primary key,
     user_id     bigserial,
     target_id   varchar(50)                         not null,
     target_type char                                not null,
@@ -99,31 +120,37 @@ create table if not exists comment
     deleted_yn  char      default 'N'::bpchar       not null,
     created_at  timestamp default CURRENT_TIMESTAMP not null,
     updated_at  timestamp default CURRENT_TIMESTAMP not null
-    );
+);
 
-comment on column comment.target_id is 'the id of the target in which current comment is subject to';
+comment on column public.comment.target_id is 'the id of the target in which current comment is subject to';
 
-comment on column comment.target_type is 'the type of the target in which current is subject to, e.g. P: POST, C: COMMENT';
+comment on column public.comment.target_type is 'the type of the target in which current is subject to, e.g. P: POST, C: COMMENT';
 
-comment on column comment.edited_yn is 'whether the comment has been edited(Y/N)';
+comment on column public.comment.edited_yn is 'whether the comment has been edited(Y/N)';
 
-comment on column comment.deleted_yn is 'whether the comment has been deleted(Y/N)';
+comment on column public.comment.deleted_yn is 'whether the comment has been deleted(Y/N)';
 
-create index if not exists idx_target_id_target_type
-    on comment (target_id, target_type);
+alter table public.comment
+    owner to seonwoo960000;
 
-create table if not exists comment_reaction
+create index idx_target_id_target_type
+    on public.comment (target_id, target_type);
+
+create table public.comment_reaction
 (
     comment_id    bigserial,
     user_id       bigserial,
     reaction_type char                                not null,
     created_at    timestamp default CURRENT_TIMESTAMP not null,
     primary key (comment_id, user_id)
-    );
+);
 
-comment on column comment_reaction.reaction_type is 'L: LIKE, D: DISLIKE';
+comment on column public.comment_reaction.reaction_type is 'L: LIKE, D: DISLIKE';
 
-create table if not exists comment_statistic
+alter table public.comment_reaction
+    owner to seonwoo960000;
+
+create table public.comment_statistic
 (
     comment_id    bigserial,
     like_count    integer default 0 not null,
@@ -131,11 +158,12 @@ create table if not exists comment_statistic
     reply_count   integer default 0 not null
 );
 
-comment on column comment_statistic.like_count is 'number of likes';
+comment on column public.comment_statistic.like_count is 'number of likes';
 
-comment on column comment_statistic.dislike_count is 'number of dislikes';
+comment on column public.comment_statistic.dislike_count is 'number of dislikes';
 
-comment on column comment_statistic.reply_count is 'number of replies';
+comment on column public.comment_statistic.reply_count is 'number of replies';
 
-
+alter table public.comment_statistic
+    owner to seonwoo960000;
 
