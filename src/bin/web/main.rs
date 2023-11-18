3c86d70c261907ec883209c35456195104bafb3a
Kim Seon Woo <seonwoo960000@naver.com>
2023-11-18 18:08:45 +0900

    Initialize database

--- src/bin/web/main.rs
+++ src/bin/web/main.rs
@@ -1,15 +1,47 @@
 use std::net::SocketAddr;
-use community::adapters::routers::router;
+use std::sync::Arc;
+use axum::handler::Handler;
+use axum::Router;
+use axum::routing::get;
+use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use community::adapters::routers::{root};
+
+pub struct AppState {
+    db: Pool<Postgres>,
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+    dotenv().ok();
 
-    let app = router();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("✅Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            println!("🔥 Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
+
+    let app = create_router(Arc::new(AppState { db: pool.clone() }));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+pub fn create_router(app_state: Arc<AppState>) -> Router {
+    Router::new().route("/", get(root))
+        .with_state(app_state)
 }
